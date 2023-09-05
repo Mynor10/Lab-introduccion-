3ba0658a343846196433851f0b930c77eb57e0bf
Mynor10 <144157931+Mynor10@users.noreply.github.com>
2023-09-05 17:58:08 -0600

    Add files via upload

---
+++ L1_MASC_1022223.py
@@ -0,0 +1,28 @@
+# Pensamiento computacional 
+# 5/09/23
+# Mynor Adolfo Salazar Contreras
+# Objetivo: Crear un porogramna para realizar operaciones basicas
+# Entrada 
+# Procesos 
+# Salida
+print ("Mi primer program")
+
+Numero1= int(input ("Seleccione el numero  5"))
+Numero2= int(input ("Seleccione el numero  2"))
+
+#Suma 
+suma =  (Numero1 + Numero2)
+
+#Resta 
+resta = (Numero1 - Numero2)
+ 
+#Division 
+division = (Numero1 / Numero2)
+
+#Multiplicacion 
+multiplicacion = (Numero1 * Numero2)
+
+print( suma )
+print( resta )
+print(division)
+print(multiplicacion)
