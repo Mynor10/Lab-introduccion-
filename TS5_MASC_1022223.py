97d9c9ff8b8612a3f21c28b787f1e40843d149ac
Mynor10 <144157931+Mynor10@users.noreply.github.com>
2023-09-07 09:41:40 -0600

    Add files via upload

---
+++ TS5_MASC_1022223.py
@@ -0,0 +1,20 @@
+# Pensamiento computacional 
+# 5/09/23
+# Mynor Adolfo Salazar Contreras
+# Objetivo: Crear variables tipi string 
+# Entrada: Nombre, edad, carreraq, carné
+# Procesos principales: pedir variable 
+# Salida: mostrar, nombre, carrera, carné 
+
+print ("mi segundo programa")
+sNombre = str(input( "Mi nombre es"))
+sEdad = str(input ("tengo"))
+sCarrera = str(input ("soy de la carrera de "))
+sCarné = str(input ("mi carné es "))
+print ("mi segundo programa")
+print ("Nombre" + sNombre)
+print ("Edad" + sEdad)
+print ("Carrera"+ sCarrera )
+print ("Carné"+ sCarné)
+
+print("Soy", sNombre , "tengo" , sEdad , "años y estudio la carrera de", sCarrera,  "mi numero de carné es", sCarné )
