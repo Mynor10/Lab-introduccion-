0949540fa0a696630b81bc8a167c401a29cbab40
Mynor10 <144157931+Mynor10@users.noreply.github.com>
2023-09-28 09:43:14 -0600

    Add files via upload

---
+++ TS8_MASC_1022223.py
@@ -0,0 +1,49 @@
+# Pensamiento computacion 
+# 21/09/23
+# Mynor Adolfo Salazar Contreras
+# Objetivo: Crear un rango del 1 al 10
+# Entrada: Nombre, edad, carreraq, carné
+# Procesos principales: pedir variable para realizar operaciones 
+# Salida: 
+
+import math 
+
+#ejercicio No.1
+print( "ejercicios for, while")
+for i in range(1,26):
+        print(i, end=",")
+print ()
+
+
+#Ejercicio No.2
+i = 1
+while i <= 26: 
+     i +=1
+     print (i, end= ",")
+print ()
+
+#Ejercicio No.3
+for i in range(5,51,5):
+    print(i, end= ",")
+    i+=5
+print()
+
+#Ejercicio No.4
+i = 5
+while i < 50: 
+     i +=5
+     print (i, end= ",")
+
+
+#Ejercicio 5
+for i in range(20,-2,-2):
+        print(i, end=",")
+print ()
+i=20
+
+#ejercicio 6
+i = 20
+while i >= 0: 
+     i -=2
+     print (i, end= ",")
+print()
